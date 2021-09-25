59bcf158e424b9f678049ada7476b3779a67158d
Shishir <shiishir@gmail.com>
2021-09-25 15:26:00 +0600

    total count and font awesome

--- src/components/Header/Product/Product.js
+++ src/components/Header/Product/Product.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 import './Product.css';
 
 const Product = (props) => {
     // console.log(props);
     const { name, img, seller, price, stock } = props.product;
+    const cartIcon = <FontAwesomeIcon icon={faShoppingCart} />
     return (
         <div className="product">
             <div>
@@ -17,7 +20,7 @@ const Product = (props) => {
                 <button
                     onClick={() => props.handleAddToCart(props.product)}
                     className="btn-regular"
-                >add to cart</button>
+                >{cartIcon}add to cart</button>
             </div>
         </div>
     );
